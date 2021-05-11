e9455d366a89ffe590445bc52acd0b06dd14aa76
gustavo2892 <gustavo2892@gmail.com>
2021-05-10 21:40:34 -0300

    Page Profile Provider in Progress

--- src/pages/Provider/index.js
+++ src/pages/Provider/index.js
@@ -3,11 +3,13 @@ import { useParams } from 'react-router-dom';
 
 import Loading from '../../components/Loading';
 import api from '../../services/api';
-// import { Container } from './styles';
+import { Container, SideMenu, ImageContainer } from './styles';
+import avatar from '~/assets/avatar.png';
 
 function Provider() {
   const [date, setDate] = useState(new Date());
   const [hours, setHours] = useState([]);
+  const [provider, setProvider] = useState({});
   const [loading, setLoading] = useState(false);
 
   const { id } = useParams();
@@ -21,6 +23,10 @@ function Provider() {
         },
       });
 
+      const responseProvider = await api.get(`providers/${id}`);
+
+      setProvider(responseProvider.data);
+
       setLoading(false);
       setHours(response.data);
     }
@@ -28,7 +34,24 @@ function Provider() {
     loadAvailable();
   }, [date, id]);
 
-  return loading ? <Loading /> : <div>Aqui será a página de profile</div>;
+  return loading ? (
+    <Loading />
+  ) : (
+    <Container>
+      <SideMenu>
+        <ImageContainer>
+          <img
+            src={(provider.avatar && provider.avatar.url) || avatar}
+            alt="Avatar"
+          />
+        </ImageContainer>
+        <p>
+          <strong>{provider.name}</strong>
+        </p>
+      </SideMenu>
+      <div>Centro da página</div>
+    </Container>
+  );
 }
 
 export default Provider;
--- src/pages/Provider/styles.js
+++ src/pages/Provider/styles.js
@@ -1,3 +1,36 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const SideMenu = styled.div`
+  height: 100%;
+  width: 250px;
+  background: #e0e0e0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    font-size: 14px;
+    margin: 15px 0px;
+    color: #007fff;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    height: 120px;
+    width: 120px;
+    border-radius: 50%;
+  }
+`;
